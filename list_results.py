f0586c32fad63c718301243baa76cdbe7742ba8a
Chuan-Zheng Lee <czlee@stanford.edu>
2021-07-04 15:35:32 +1200

    Add list_results.py

---
+++ list_results.py
@@ -0,0 +1,45 @@
+"""Summarizes results in the given results directory."""
+
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("dir", type=Path, default=Path("results"),
+    help="Results directory")
+args = parser.parse_args()
+
+resultsdir = args.dir
+
+if not resultsdir.is_dir():
+    print(f"{resultsdir} is not a directory")
+    exit(1)
+
+for child in resultsdir.iterdir():
+    date = child.name
+
+    argsfile = child / "arguments.txt"
+    if not argsfile.exists():
+        continue
+
+    with open(argsfile) as f:
+        script = None
+        commit = None
+        for line in f:
+            if line.startswith("script: "):
+                script = line[8:].strip()
+            if line.startswith("commit: "):
+                commit = line[8:15].strip()
+
+            if line.strip() == "== arguments ==":
+                break
+
+        # arguments
+        args = {}
+        for line in f:
+            key, value = line.split(sep=':', maxsplit=1)
+            key = key.strip()
+            value = value.strip()
+            args[key] = value
+        argsstring = " ".join(f"{key}={value}" for key, value in args.items())
+
+        print(f"{date} {commit} {script:<20}  {argsstring}")
