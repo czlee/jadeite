2214c169ca851165bdf6081b57d2bab3521dac54
Chuan-Zheng Lee <czlee@stanford.edu>
2021-07-14 15:11:04 +1200

    Minor reordering of lines

--- simple.py
+++ simple.py
@@ -23,11 +23,11 @@ args = parser.parse_args()
 results_dir = results.create_results_directory()
 results.log_arguments(args, results_dir)
 
+train_dataset = epsilon.EpsilonDataset(train=True, small=args.small)
+test_dataset = epsilon.EpsilonDataset(train=False, small=args.small)
 model = epsilon.EpsilonLogisticModel()
 loss_fn = torch.nn.functional.binary_cross_entropy
 metric_fns = {"accuracy": metrics.binary_accuracy}
-train_dataset = epsilon.EpsilonDataset(train=True, small=args.small)
-test_dataset = epsilon.EpsilonDataset(train=False, small=args.small)
 
 experiment = SimpleExperiment.from_arguments(
     (train_dataset, test_dataset), model, loss_fn, metric_fns, results_dir, args)
