66f3c5733c5cffb9dc2e71c004dfe29f77f1c7ba
Chuan-Zheng Lee <czlee@stanford.edu>
2021-07-21 00:28:01 +1200

    Modify stocquant.py to be similar to ota-vs-clients.py

--- stocquant.py
+++ stocquant.py
@@ -1,4 +1,4 @@
-"""Basic logistic regression using federated averaging."""
+"""Stochastic quantization implementation of federated averaging."""
 
 # Chuan-Zheng Lee <czlee@stanford.edu>
 # July 2021
@@ -15,22 +15,35 @@ import results
 from experiments import SimpleQuantizationFederatedExperiment
 
 
-parser = argparse.ArgumentParser(description=__doc__)
+parser = argparse.ArgumentParser(description=__doc__, conflict_handler='resolve')
 SimpleQuantizationFederatedExperiment.add_arguments(parser)
 parser.add_argument("--small", action="store_true", default=False,
     help="Use a small dataset for testing")
+parser.add_argument("-n", "--clients", type=int, nargs='+', default=[10],
+    help="Number of clients, n")
+parser.add_argument("-q", "--repeat", type=int, default=1,
+    help="Number of times to repeat experiment")
 args = parser.parse_args()
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s | %(message)s")
-results_dir = results.create_results_directory()
-results.log_arguments(args, results_dir)
+top_results_dir = results.create_results_directory()
+results.log_arguments(args, top_results_dir)
 
 train_dataset = epsilon.EpsilonDataset(train=True, small=args.small)
 test_dataset = epsilon.EpsilonDataset(train=False, small=args.small)
 loss_fn = torch.nn.functional.binary_cross_entropy
 metric_fns = {"accuracy": metrics.binary_accuracy}
 
-experiment = SimpleQuantizationFederatedExperiment.from_arguments(
-    train_dataset, test_dataset, epsilon.EpsilonLogisticModel,
-    loss_fn, metric_fns, results_dir, args)
-experiment.run()
+nclients_list = args.clients
+
+for i in range(args.repeat):
+    for n in nclients_list:
+        args.clients = n
+        print(f"=== Iteration {i} of {args.repeat}, {n} clients ===")
+        results_dir = top_results_dir / f"clients-{n}-iteration-{i}"
+        results_dir.mkdir()
+        results.log_arguments(args, results_dir, other_info={'iteration': i, 'clients': n})
+        experiment = SimpleQuantizationFederatedExperiment.from_arguments(
+            train_dataset, test_dataset, epsilon.EpsilonLogisticModel,
+            loss_fn, metric_fns, results_dir, args)
+        experiment.run()
