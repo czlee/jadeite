czlee/jadeite

commit f04c74d1ca10c75a77a703d6dca74b80bd3bd143

Change learning rate short argument to -lr

experiments/experiment.py
@@ -241,7 +241,7 @@ class SimpleExperiment(BaseExperiment):
 
     @classmethod
     def add_arguments(cls, parser: argparse.ArgumentParser):
-        parser.add_argument("-l", "--lr", "--learning-rate", type=float, default=1e-2,
+        parser.add_argument("-l", "-lr", "--learning-rate", type=float, default=1e-2,
             help="Learning rate")
         super().add_arguments(parser)
 

experiments/federated.py
@@ -98,7 +98,7 @@ class BaseFederatedExperiment(BaseExperiment):
             help="Number of rounds")
         federated_args.add_argument("-n", "--clients", type=int,
             help="Number of clients, n")
-        federated_args.add_argument("-l", "--lr-client", type=float, default=1e-2,
+        federated_args.add_argument("-lr", "--lr-client", type=float, default=1e-2,
             help="Learning rate at client")
         federated_args.add_argument("-dpc", "--data-per-client", type=int, default=None,
             help="Override the number of data points each client has (default: "

commit d6c65f1306f7f4c7d6cc62da87da8e03d4b79602

Fix bug introduced in f04c74d

experiments/experiment.py
@@ -261,7 +261,7 @@ class SimpleExperiment(BaseExperiment):
         the caller --- they're too complicated to try to "generalize".
         """
         device = cls._interpret_cpu_arg(args.cpu)
-        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate)
         params = cls.extract_params_from_args(args)
         return cls(train_dataset, test_dataset, model, loss_fn, metric_fns,
                    optimizer, results_dir, device, **params)