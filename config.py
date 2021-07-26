8a327c5064319cf1444bcabe1364948c9648f200
Chuan-Zheng Lee <czlee@stanford.edu>
2021-07-26 15:40:04 +1200

    Update comments in config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 """Configuration.
 
-Reads the config.json file, and sets relevant attributes of itself accordingly.
+Reads the config.yaml file, and sets relevant attributes of itself accordingly.
 Configuration variables should be accessed as, for example:
 
     from config import DATA_DIRECTORY, RESULTS_DIRECTORY
+
+If config.yaml isn't configured correctly, it exits completely upon import.
 
 This uses some Python magic, but hopefully not too obscure. Its purpose is to
 try to allow imports of the style above, without having to catch `ImportError`
@@ -38,7 +40,8 @@ if _config_file_dict is None:
     print("values for your machine.")
     exit(1)
 
-# All options are in a top-level "config"
+# All options are in a top-level "config" to allow space to add other top-level
+# items, e.g. default arguments, later.
 try:
     _config_options = _config_file_dict["config"]
 except KeyError:
@@ -48,7 +51,7 @@ except KeyError:
 _missing_names = set(_required_config_names) - set(_config_options.keys())
 
 if _missing_names:
-    print("The following required config settings are missing from config.json:")
+    print("The following required config settings are missing from config.yaml:")
     for _name in _missing_names:
         print(" - " + _name)
     exit(1)
